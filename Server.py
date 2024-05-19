f2dd642431511a0968698c76bd5d44e6806f2e7d
Alisa-Shala <116600381+Alisa-Shala@users.noreply.github.com>
2024-05-19 02:49:50 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -12,3 +12,14 @@ keyFile = open("/Users/anditernava/server.key").read()
 
 x509 = openssl.load_certificate(openssl.FILETYPE_PEM, certiFile)
 key = openssl.load_privatekey(openssl.FILETYPE_PEM, keyFile)
+# Create server socket
+try:
+    serverSocket = soct.socket(soct.AF_INET, soct.SOCK_STREAM)
+    print("Socket successfully created")
+except soct.error as err:
+    print("Socket creation failed with error", err)
+
+serverPort = 8000
+serverSocket.bind(('127.0.0.1', serverPort))
+serverSocket.listen(5)
+print("Socket is listening")
